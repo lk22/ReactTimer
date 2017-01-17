9a69af96741a71b82b87cdc2fe5d202da028ec0b
Leo Knudsen <nightskater-220@hotmail.com>
2017-01-17 23:36:51 +0100

    test countdown component

--- app/resources/assets/js/components/Countdown.jsx
+++ app/resources/assets/js/components/Countdown.jsx
@@ -3,24 +3,50 @@ var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
-  getInitialState: function () {
-    return {count: 0};
-  },
-  handleSetCountdown: function (seconds) {
-    this.setState({
-      count: seconds
-    });
-  },
-  render: function () {
-    var {count} = this.state;
+	getInitialState: function () {
+		return {count: 0, countdownStatus: 'stopped'};
+	},
 
-    return (
-      <div>
-        <Clock totalSeconds={count}/>
-        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
-      </div>
-    );
-  }
+	componentDidUpdate: function(prevProps, prevState) {
+		if(this.state.countdownStatus !== prevState.countdownStatus) {
+			switch (this.state.countdownStatus) {
+				case 'started':
+					this.startTimer(); // start the timer
+					break;
+				default:
+					// do nothing
+					break;
+			}
+		}
+	},
+
+	startTimer: function() {
+		this.timer = setInterval(() => {
+			var newCount = this.state.count - 1;
+
+			this.setState({
+				count: newCount >= 0 ? newCount : 0
+			});
+
+		}, 1000);
+	},
+
+	handleSetCountdown: function (seconds) {
+	    this.setState({
+	      count: seconds,
+	      countdownStatus: 'started'
+	    });
+	},
+	render: function () {
+	    var {count} = this.state;
+
+	    return (
+	      <div>
+	        <Clock totalSeconds={count}/>
+	        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+	      </div>
+	    );
+	}
 });
 
 module.exports = Countdown;
---
+++ app/resources/assets/js/tests/components/Countdown.test.jsx
@@ -0,0 +1,49 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jQuery');
+var expect = require('expect');
+
+var Countdown = require('Countdown');
+
+describe('Countdown component', () =>
+{
+	it('should exist', () =>
+   {
+   		expect(Countdown).toExist();
+   });
+
+	describe('Handle countdown method', () =>
+	{
+		it('should set state to started and countdown', (done) =>
+		{
+			var countdown = TestUtils.renderIntoDocument(<Countdown />);
+
+			countdown.handleSetCountdown(10);
+
+			expect(countdown.state.count).toBe(10);
+			expect(countdown.state.countdownStatus).toBe('started');
+
+			setTimeout(() =>
+			{
+				expect(countdown.state.count).toBe(9);
+				done();
+			}, 1001);
+		});
+
+		it('should prevent count state to hit negative number while counting', (done) =>
+	   {
+	   		var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+
+	   		countdown.handleSetCountdown(1);
+
+	   		expect(countdown.state.count).toBe(1);
+
+	   		setTimeout(() =>
+	   		{
+	   			expect(countdown.state.count).toBe(0);
+	   			done();
+	   		}, 3001);
+	   });
+	});
+});
